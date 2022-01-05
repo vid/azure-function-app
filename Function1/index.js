790d32c06554f822a9af06268763d212aceb907b
vid <vid_github@zooid.org>
2022-01-05 09:01:56 -0500

    access page

--- Function1/index.js
+++ Function1/index.js
@@ -1,19 +1,18 @@
-const { getOptionsOrDefault } = require('@haibun/core/build/lib/util');
-const { runWith } = require('@haibun/core/build/lib/run');
-const { getDefaultWorld, asFeatures, } = require('@haibun/core/build/lib/test/lib');
 
+const WebPlaywright = require('@haibun/web-playwright/build/web-playwright');
+const DomainWebpage = require('@haibun/domain-webpage/build/domain-webpage');
+
+const { testWithDefaults } = require('@haibun/core/build/lib/test/lib');
+
+const inFeatures = [{ path: '/features/test.feature', content: `On the https://www.cbc.ca webpage` }];
 module.exports = async function (context, req) {
-    let body = 'no directive';
-    if (context.query.run) {
-        const specl = getOptionsOrDefault();
-
-        const { world } = getDefaultWorld(0);
-
-        const features = asFeatures(inFeatures);
-        const backgrounds = asFeatures([]);
-
-        const res = await runWith({ specl, features, backgrounds, addSteppers: [], world, extraOptions: {} });
-        body = JSON.stringify(res);
+    if (req.query.run) {
+        process.env.HAIBUN_LOG_LEVEL = 'debug';
+        const addSteppers = [WebPlaywright.default, DomainWebpage.default];
+        const { result } = await testWithDefaults(inFeatures, addSteppers)
+        body = result;
+    } else {
+        body = "no directive"
     }
     context.log({ body });
     context.res = { body }
