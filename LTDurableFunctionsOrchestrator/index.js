c14dda3f7610bf9bc2795c2f492df841cc99f65f
vid <vid_github@zooid.org>
2022-01-27 15:56:59 -0500

    add durable function

---
+++ LTDurableFunctionsOrchestrator/index.js
@@ -0,0 +1,15 @@
+﻿/*
+ * This function will be triggered by an HTTP starter function.
+ */
+
+const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+    const outputs = [];
+
+    for (let i = 0; i < 10; i++) {
+    outputs.push(yield context.df.callActivity("LTActivity", i));
+    }
+
+    return outputs;
+});
